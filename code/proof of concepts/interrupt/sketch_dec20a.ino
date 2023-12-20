0008f59570dd875fb4dc3aaed5c73f9d1797aad7
BasMaes <146442899+BasMaes@users.noreply.github.com>
2023-12-20 14:59:28 +0100

    Add files via upload

---
+++ code/proof of concepts/interrupt/sketch_dec20a.ino
@@ -0,0 +1,25 @@
+int led = 3;
+int interruptPin = 2;
+bool state = LOW;
+
+void setup()
+{
+  pinMode(interruptPin, INPUT);
+  pinMode(led, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interrupt, RISING);
+}
+
+void loop()
+{
+  digitalWrite(led, state);
+}
+
+void interrupt()
+{
+  static unsigned long vorigeInterrupt = 0;
+  unsigned long interruptTijd = millis();
+  if (interruptTijd - vorigeInterrupt > 100){
+    state =! state;
+  }
+  vorigeInterrupt = interruptTijd;
+}
